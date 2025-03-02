d527b7ef85c042c9b7f3835fc9e45738f0e59de8
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2025-03-02 15:32:03 +0100

    finished header

---
+++ src/js/swiper.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.custom-next',
+    prevEl: '.custom-prev',
+  },
+  allowTouchMove: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    425: {
+      slidesPerView: 2,
+    },
+    765: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+    1440: {
+      slidesPerView: 5,
+    },
+  },
+});
